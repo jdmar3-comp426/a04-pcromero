2935e266ed835eb622b972619658546a172a2a79
Peter Romero <peter@Peters-MacBook-Pro-2.local>
2021-12-04 19:03:18 -0500

    fixed message in delete

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.get("/app/", (req, res, next) => {
 app.post("/app/new/", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user,pass) VALUES (?, ?)");
 	const info = stmt.run(req.body.user, md5(req.body.pass));
-	res.status(201).json({"message": info.changes+ " record created: ID " + info.lastInsertRowid + " (201)"});
+	res.status(201).json({"message": info.changes + " record created: ID " + info.lastInsertRowid + " (201)"});
 })
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users/", (req, res) => {	
@@ -50,7 +50,7 @@ app.patch("/app/update/user/:id", (req, res) => {
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
 	const info = stmt.run(req.params.id);
-	res.status(200).json({"message": info.changes + " record deleted: ID " + req.params.id + " 200"});
+	res.status(200).json({"message": info.changes + " record deleted: ID " + req.params.id + " (200)"});
 })
 
 // Default response for any other request
